0e6458ea64d81af02fd98c19c2b811c480af4381
renanfortkamp <renan_fortkamp@estudante.sesisenai.org.br>
2023-02-05 21:38:14 -0300

    dto para retornar apenas nome e nome da permissão

---
+++ Dto/NomePermissao.cs
@@ -0,0 +1,15 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using webapi_security.Models;
+
+namespace webapi_security.Dto
+{
+    public class NomePermissao
+    {
+        public string Nome { get; set; }
+
+        public string Permissao { get; set; }
+    }
+}
